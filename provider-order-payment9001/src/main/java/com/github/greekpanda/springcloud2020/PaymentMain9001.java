1ee2e2526e33593d700cc145c0c32406bf4b7442
liaohuanghe <liaohuanghe@halobop.com>
2020-04-23 16:28:47 +0800

    1.添加了测试模块

--- provider-order-payment9001/src/main/java/com/github/greekpanda/springcloud2020/PaymentMain9001.java
+++ provider-order-payment9001/src/main/java/com/github/greekpanda/springcloud2020/PaymentMain9001.java
@@ -1,9 +1,11 @@
 package com.github.greekpanda.springcloud2020;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class PaymentMain9001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain9001.class, args);
---
+++ provider-order-payment9001/src/test/java/com/github/greekpanda/springcloud2020/service/impl/PaymentServiceImplTest.java
@@ -0,0 +1,26 @@
+package com.github.greekpanda.springcloud2020.service.impl;
+
+import com.github.greekpanda.springcloud2020.entities.Payment;
+import com.github.greekpanda.springcloud2020.service.IPaymentService;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PaymentServiceImplTest {
+
+    @Autowired
+    public IPaymentService paymentService;
+
+    @Test
+    public void create() {
+//        String serial = "test11111111";
+//        Payment payment = new Payment();
+//        payment.setSerial(serial);
+//        paymentService.create(payment);
+
+    }
+
+    @Test
+    public void getPaymentById() {
+    }
+}
