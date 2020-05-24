3cb001a27fc690e50fcc205952befc5d2e9477ef
liaohuanghe <271799648@qq.com>
2020-05-24 18:20:04 +0800

    1.新增了3366的配置客户端，用于测试消息总线上的配置生效

---
+++ cloud-config-client3366/src/main/java/com/github/greekpanda/springcloud2020/ConfigClient3366.java
@@ -0,0 +1,16 @@
+package com.github.greekpanda.springcloud2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/5/24 18:14
+ */
+@SpringBootApplication
+public class ConfigClient3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366.class, args);
+    }
+}
---
+++ cloud-config-client3366/src/main/java/com/github/greekpanda/springcloud2020/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.github.greekpanda.springcloud2020.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/5/24 18:16
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo" + configInfo;
+    }
+
+}
