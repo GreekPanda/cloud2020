506adae4d64b5091667d6b611187a795f3952b9f
liaohuanghe <271799648@qq.com>
2020-05-24 12:20:23 +0800

    1.添加了Gateway模块，使用Gateway 9527端口即可访问原来直接访问的服务，比如9001

---
+++ cloud-gateway-gateway9527/src/main/java/com/github/greekpanda/springcloud2020/Gateway9527.java
@@ -0,0 +1,16 @@
+package com.github.greekpanda.springcloud2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/5/24 11:58
+ */
+@SpringBootApplication
+public class Gateway9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway9527.class, args);
+    }
+}
