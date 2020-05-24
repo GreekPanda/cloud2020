c9f7737f20777643bbfdf66b1aad081e2a814610
liaohuanghe <271799648@qq.com>
2020-05-24 11:07:22 +0800

    1.增加了hystrix dashboard模块用于监控hystrix作用的模块和方法

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/github/greekpanda/springcloud2020/HystrixDashboard9001.java
@@ -0,0 +1,18 @@
+package com.github.greekpanda.springcloud2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/5/24 10:39
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboard9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9001.class, args);
+    }
+}
