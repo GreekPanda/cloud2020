f4a269c69f56ebb650bd162c6baa5f1c5ed36842
liaohuanghe <271799648@qq.com>
2020-05-25 08:24:25 +0800

    1.新建了基于nacos的消费者模块，用于测试nacos的注册发现

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/github/greekpanda/cloudalibaba2020/OrderNacosMain83.java
@@ -0,0 +1,18 @@
+package com.github.greekpanda.cloudalibaba2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/5/25 08:20
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
