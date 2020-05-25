23693e25cfa811498941edc8652b31c16f6278ca
liaohuanghe <271799648@qq.com>
2020-05-25 08:07:44 +0800

    1.新建了基于nacos的服务提供者

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/github/greekpanda/cloudalibaba2020/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.github.greekpanda.cloudalibaba2020.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/5/25 08:04
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Long id) {
+        return "hello from nacos " + port + " id " + id;
+    }
+}
