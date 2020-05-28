a15aca1ad7a1e812e5e8835a7a40d9988cf222f5
liaohuanghe <271799648@qq.com>
2020-05-28 13:03:29 +0800

    1.对于热点的访问进行了限流，包括对于热点参数和兜底处理方法

--- cloudalibaba-sentinel-service8401/src/main/java/com/github/greekpanda/cloud2020/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/github/greekpanda/cloud2020/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.github.greekpanda.cloud2020.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,5 +23,48 @@ public class FlowLimitController {
     @GetMapping(value = "/testB")
     public String testB() {
         return "========testB";
+    }
+
+    @GetMapping(value = "/testD")
+    public String testD() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("==========testD========");
+        return " thread :" + Thread.currentThread().getName() + " testD: ";
+    }
+
+    @GetMapping(value = "/testE")
+    public String testE() {
+        int i = 10 / 0;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("==========testD========");
+        return " thread :" + Thread.currentThread().getName() + " testD: ";
+    }
+
+    @GetMapping(value = "/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+
+        return "testHotKey: p1= " + p1 + " p2= " + p2;
+    }
+
+    public String dealHotKey(String p1, String p2, BlockException be) {
+        return "dealHotKey";
+    }
+
+    @GetMapping(value = "/testHotKey1")
+    @SentinelResource(value = "testHotKey") //no BlockHandler
+    public String testHotKey1(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+
+        return "testHotKey: p1= " + p1 + " p2= " + p2;
     }
 }
